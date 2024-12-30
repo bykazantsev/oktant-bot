a5bc1467a545fad3b95261611ed701843f331eea
bykazantsev <vovaytgame1@gmail.com>
2024-12-30 20:44:38 +0500

    Переименовал файл, добавил класс к ctx для подсказок в IDE

--- cogs/punishments.py
+++ cogs/punishments.py
@@ -1,13 +1,13 @@
 import discord
 from discord.ext import commands
-from db.database import PunishmentsDatabase
-from embeds import punishments as embeds
+from db.punishments import PunishmentsDatabase
+from locales import embeds
 from utils.logger import Logger
 
 logger = Logger.get_logger()
 
 
-class BanCog(commands.Cog):
+class PunishmentsCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.db = PunishmentsDatabase()
@@ -15,7 +15,7 @@ class BanCog(commands.Cog):
     @commands.slash_command(description="Забанить пользователя")
     async def ban(
             self,
-            ctx,
+            ctx: discord.ApplicationContext,
             user: discord.Option(discord.SlashCommandOptionType.user,
                                  description="Пользователь, которого нужно забанить"),
             reason: discord.Option(str, description="Причина бана"),
@@ -32,10 +32,9 @@ class BanCog(commands.Cog):
 
         self.db.add_punishment(user.id, "ban", reason, duration)
         logger.info(f"{ctx.author.name} забанил {user.name} на {duration} по причине: {reason}")
-
+        await ctx.edit(embed=embeds.mod_ban(duration, reason))
         try:
             await user.send(embed=embeds.user_ban(duration, reason, ctx.author.id))
-            await ctx.edit(embed=embeds.mod_ban(duration, reason))
         except discord.errors.Forbidden:
             error_message = "Не удалось отправить сообщение в ЛС"
             logger.info(
@@ -46,8 +45,8 @@ class BanCog(commands.Cog):
             logger.error(f"Сообщение о блокировке {user.name} ({user.id}) не получилось отправить в ЛС ({e})")
             await ctx.edit(
                 embeds=[embeds.mod_ban(duration, reason), embeds.error(str(e), "Не удалось отправить сообщение в ЛС")])
-        # finally:
-        #     await user.ban()
+        finally:
+            await user.ban()
 
 def setup(bot):
-    bot.add_cog(BanCog(bot))
+    bot.add_cog(PunishmentsCog(bot))
