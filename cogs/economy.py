24f9cb0b2c5aebdcd669211a1a56296ca19aa685
bykazantsev <vovaytgame1@gmail.com>
2024-12-30 20:47:08 +0500

    Добавил много команд для работ с валютами

--- cogs/economy.py
+++ cogs/economy.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
-from db.database import EconomyDatabase
+from db.economy import EconomyDatabase
+from locales import embeds
 from utils.logger import Logger
 
 logger = Logger.get_logger()
@@ -10,12 +11,12 @@ class EconomyCog(commands.Cog):
         self.bot = bot
         self.db = EconomyDatabase()
 
-    @commands.slash_command(description="Выдать определенное количество валюты в боте")
+    @commands.slash_command(description="Выдать определенное количество валюты у пользователя")
     async def add_balance(
             self,
-            ctx,
+            ctx: discord.ApplicationContext,
             user: discord.Option(discord.SlashCommandOptionType.user,
-                                 description="Пользователь, которому нужно выдать валюты"),
+                                 description="Пользователь, которому нужно выдать валюту"),
             money: discord.Option(int, description="Сколько выдать")
     ):
         await ctx.response.defer(ephemeral=True)
@@ -23,8 +24,49 @@ class EconomyCog(commands.Cog):
         if ctx.guild is None:
             await ctx.edit(content="Команду нельзя использовать в ЛС!")
             return
-        self.db.update_balance(user.id, money)
-        await ctx.edit(content=f"Выдали {money} валюты {user.mention}")
+
+        self.db.update_balance(user.id, ctx.guild.id, money, "admin_add")
+        balance = self.db.get_balance(user.id)
+        await ctx.edit(embed=embeds.green("Выдача валюты", f"Вы выдали {user.mention} {money} валюты.\nТекущий баланс: {balance}"))
+
+
+    @commands.slash_command(description="Вычесть определенное количество валюты у пользователя")
+    async def minus_balance(
+            self,
+            ctx: discord.ApplicationContext,
+            user: discord.Option(discord.SlashCommandOptionType.user,
+                                 description="Пользователь, которому нужно вычесть валюту"),
+            money: discord.Option(int, description="Сколько выдать")
+    ):
+        await ctx.response.defer(ephemeral=True)
+
+        if ctx.guild is None:
+            await ctx.edit(content="Команду нельзя использовать в ЛС!")
+            return
+
+        if money == 0:
+            money = self.db.get_balance(user.id, ctx.guild.id)
+
+        self.db.minus_balance(user.id, ctx.guild.id, money, "admin_minus")
+        balance = self.db.get_balance(user.id, ctx.guild.id)
+        await ctx.edit(embed=embeds.red("Вычитание валюты", f"Вы удалили {money} валюты у {user.mention}.\nТекущий баланс: {balance}"))
+
+    @commands.slash_command(description="Установить определенное количество валюты у пользователя")
+    async def set_balance(
+            self,
+            ctx: discord.ApplicationContext,
+            user: discord.Option(discord.SlashCommandOptionType.user,
+                                 description="Пользователь, которому нужно установить валюту"),
+            money: discord.Option(int, description="Количество валюты")
+    ):
+        await ctx.response.defer(ephemeral=True)
+
+        if ctx.guild is None:
+            await ctx.edit(content="Команду нельзя использовать в ЛС!")
+            return
+
+        self.db.set_balance(user.id, ctx.guild.id, money, "admin_set")
+        await ctx.edit(embed=embeds.green("Выдача валюты", f"Вы установили {money} валюты у {user.mention}."))
 
 
 def setup(bot):
