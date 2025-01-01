a3c0c6a65fb7257870df78fd10b8efd4721281a9
bykazantsev <vovaytgame1@gmail.com>
2025-01-01 17:21:15 +0500

    Упростил использование кода

--- locales/embeds.py
+++ locales/embeds.py
@@ -1,21 +1,30 @@
 import discord
 from datetime import datetime
 
-def _user_punishment(duration, reason, mod_id, guild_name, guild_icon, title="Вам было выдано наказание!"):
+
+def create_punishment(user_or_mod, action, duration, reason, guild_name: str, guild_icon: str, mod_id=None):
+    titles = {
+        "user": {
+            "ban": "Вам была выдана блокировка!",
+            "mute": "Вам была выдана блокировка чата!"
+        },
+        "mod": {
+            "ban": "Вы выдали блокировку!",
+            "mute": "Вы выдали блокировку чата!"
+        }
+    }
+
+    title = titles[user_or_mod][action]
     embed = discord.Embed(title=title, color=discord.Color.red())
     embed.set_author(name=guild_name, icon_url=guild_icon)
     embed.add_field(name="Срок наказания:", value=duration, inline=True)
     embed.add_field(name="Причина:", value=reason, inline=True)
-    embed.add_field(name="ID модератора для жалобы:", value=mod_id, inline=True)
-    embed.set_footer(text=f"Oktant Bot, {datetime.now().year}")
-    return embed
 
-def _mod_punishment(duration, reason, guild_name, guild_icon, title="Вы выдали наказание!"):
-    embed = discord.Embed(title=title, color=discord.Color.red())
-    embed.set_author(name=guild_name, icon_url=guild_icon)
-    embed.add_field(name="Срок:", value=duration, inline=True)
-    embed.add_field(name="Причина:", value=reason, inline=True)
+    if user_or_mod == "user":
+        embed.add_field(name="ID модератора для жалобы:", value=mod_id, inline=True)
+
     embed.set_footer(text=f"Oktant Bot, {datetime.now().year}")
+
     return embed
 
 def green(title, description):
@@ -29,14 +38,15 @@ def red(title, description):
     embed.set_footer(text=f"Oktant Bot, {datetime.now().year}")
     return embed
 
-def user_punishment_end(punishment_type, guild_name, guild_icon):
-    punishment_titles = {
+
+def punishment_end(punishment_type, guild_name, guild_icon):
+    titles = {
         "ban": "Ваша блокировка закончилось!",
         "mute": "Ваша блокировка чата закончилось!",
         "warn": "Ваше предупреждение закончилось!"
     }
 
-    title = punishment_titles.get(punishment_type, "Ваше наказание закончилось!")
+    title = titles.get(punishment_type, "Ваше наказание закончилось!")
     embed = discord.Embed(title=title, description="Хорошего дня, не нарушайте больше!", color=discord.Color.green())
     embed.set_author(name=guild_name, icon_url=guild_icon)
     embed.set_footer(text=f"Oktant Bot, {datetime.now().year}")
@@ -50,16 +60,3 @@ def error(error_text, where):
     embed.add_field(name="Подробности:", value=error_text, inline=True)
     embed.set_footer(text=f"Oktant Bot, {datetime.now().year}")
     return embed
-
-
-def user_ban(duration, reason, mod_id, guild_name, guild_icon):
-    return _user_punishment(duration, reason, mod_id, guild_name, guild_icon, title="Вам была выдана блокировка!")
-
-def user_mute(duration, reason, mod_id, guild_name, guild_icon,):
-    return _user_punishment(duration, reason, mod_id, guild_name, guild_icon, title="Вам была выдана блокировка чата!")
-
-def mod_ban(duration, reason, guild_name, guild_icon):
-    return _mod_punishment(duration, reason, guild_name, guild_icon, title="Вы выдали блокировку!")
-
-def mod_mute(duration, reason, guild_name, guild_icon,):
-    return _mod_punishment(duration, reason, guild_name, guild_icon, title="Вы выдали блокировку чата!")
